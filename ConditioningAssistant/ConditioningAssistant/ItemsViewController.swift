d8687e9de3bbc10c43fda56553cef8c7cfe81b48
WyattZhang <zwuquan@gmail.com>
2015-06-14 22:57:03 +0800

    2-Level Tree TableView

--- ConditioningAssistant/ConditioningAssistant/ItemsViewController.swift
+++ ConditioningAssistant/ConditioningAssistant/ItemsViewController.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
+    
+    var isFold = [true, true]
+    let sectionTitleArr = ["Section 1", "Section 2"]
+    let sectionListArr = [["New Zealand","Australia","Bangladesh","Sri Lanka"], ["India","South Africa","UAE","Pakistan"]]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        
         self.title = "All Items"
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
     }
 
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -31,80 +31,64 @@ class ItemsViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 2
+        println("numberOfSections")
+        return sectionTitleArr.count
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 50
+        println("numberOfRows In section")
+        if (isFold[section] == false) {
+            return sectionListArr[section].count
+        }
+        
+        return 0
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
+        
+        if !isFold[indexPath.section] {
+            cell.textLabel?.text = sectionListArr[indexPath.section][indexPath.row]
+            cell.backgroundColor = UIColor.greenColor()
+        }
+        
         return cell
     }
     
     override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 100
+        return 50.0
     }
 
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "ABC"
+        return sectionTitleArr[section]
     }
-
-    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 80
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRectMake(0.0, 0.0, tableView.frame.size.width, 50.0))
+        headerView.backgroundColor = UIColor.blueColor()
+        let headerString = UILabel(frame: CGRect(x: 10, y: 10, width: tableView.frame.size.width-10, height: 30)) as UILabel
+        headerString.text = sectionTitleArr[section]
+        headerView .addSubview(headerString)
+        return headerView
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tag = section
+        println(view.tag)
+        let headerTapped = UITapGestureRecognizer(target: self, action: "sectionHeaderTapped:")
+        view.addGestureRecognizer(headerTapped)
 
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    
+    func sectionHeaderTapped(recognizer: UITapGestureRecognizer) {
+        println("Tapping working")
+        println(recognizer.view?.tag)
+        
+        let tappedSection = recognizer.view?.tag as Int!
+        isFold[tappedSection] = !isFold[tappedSection]
+        var sectionToReload = NSIndexSet(index: tappedSection)
+        self.tableView.reloadSections(sectionToReload, withRowAnimation: UITableViewRowAnimation.Fade)
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
